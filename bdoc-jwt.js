ac573d0e33f25554681aeb74a628326d2f807420
mogwaee <mogwaee@gmail.com>
2022-06-17 14:05:30 +0200

    update to integrate passing user via params, added port in env

--- bdoc-jwt.js
+++ bdoc-jwt.js
@@ -6,7 +6,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-app.listen(2000);
+app.listen(process.env.PORT);
 app.use(express.json());
 
 const nowTime = () => {
@@ -15,25 +15,37 @@ const nowTime = () => {
 };
 
 console.log(nowTime() + " Server startup...");
-console.log(nowTime() + " Server ready: listening on port 2000.");
+console.log(nowTime() + " Server ready: listening on port " + process.env.PORT);
 
 app.post("/jwt", async (req, res) => {
     // Pointless to hash, but what the heck!
     //const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const hashedPassword = await bcrypt.hash(process.env.PASSWORD, 10);
 
-    const user = {
-        user: req.body.user,
+    const storedUser = {
+        username: process.env.BDOCUSER,
         password: hashedPassword,
     };
 
-    if (user.user != process.env.BDOCUSER) {
+    const user = {};
+    // Check if user and password are passed via query parameters
+    if (req.query.username != null) {
+        //console.log("User passed as parameter: " + req.query.username);
+        user.username = req.query.username;
+        user.password = req.query.password;
+    } else {
+        //console.log("User passed in body: " + req.body.username);
+        user.username = req.body.username;
+        user.password = req.body.password;
+    }
+
+    if (user.username != process.env.BDOCUSER) {
         console.log("Unknown user");
         return res.status(400).send({ error: "Unknown user" });
     }
 
     try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
+        if (await bcrypt.compare(user.password, storedUser.password)) {
             let payload = {
                 iss: process.env.ISSUER,
                 aud: process.env.AUDIENCE,
@@ -49,7 +61,15 @@ app.post("/jwt", async (req, res) => {
             // Create JWT
             const accessToken = await jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, options);
             res.json({ accessToken: accessToken });
+
+            // Decode JWT in order to determine exp date
+            const { exp } = jwt.decode(accessToken);
+            let expiresOn = new Date(exp * 1000);
+
             console.log(nowTime() + " Generated JWT");
+            console.log("Access Token: " + accessToken);
+            console.log(`Token expires on: ${expiresOn.toLocaleDateString()} ${expiresOn.toLocaleTimeString()}`);
+            console.log(`\n${nowTime()} Server ready: listening on port ${process.env.PORT}`);
         } else {
             res.send({ error: "Wrong password" });
         }
